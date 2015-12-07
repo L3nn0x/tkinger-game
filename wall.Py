75c4746dd47dc3d952a6e4ec2886c878bff025ec
A3-167Bo213 <A3-167Bo213@users.noreply.github.com>
2015-12-07 10:19:22 +0100

    Update wall.Py

--- wall.Py
+++ wall.Py
@@ -1,25 +1,34 @@
+from glob import *
+
 class Wall(Entity):
 	"""docstring for Wall"""
 	def __init__(self,screen,pos,nb):
 		filename = "wall.bnp"
 		super().__init__(screen,filename,pos)
 
-
 class Red(self):
 	"""docstring for Red"""
-	def __init__(self,red,):
+	def __init__(self,screen,pos):
 		filename = "red.bnp"
 		super().__init__(screen,filename,pos)
-			
 
+	def update(self,dt,level):
+		import random
+		directions = [NORTH, SOUTH, WEST, EAST]
+		typ = super().move(directions[random.randInt(0, 3)], level)
+		while typ == WALL:
+			if typ == PERSO:
+				raise ValueError
+			typ = super().move(directions[random.randInt(0, 3)], level)
+			
 class Green(self):
 	"""docstring for green"""
-	def __init__(self, green):
+	def __init__(self,screen,pos):
 		filename = "green.bnp"
 		super().__init__(screen,filename,pos)
 
 class Exit(self):
 	"""docstring for Exit"""
-	def __init__(self,exit):
+	def __init__(self,screen,pos):
 		filename = "exit.bnp"
 		super().__init__(screen,filename,pos)
