c0d6905d51b4dbcf37da8da832d4bf48514ac991
Zaru <mathieu.tu@gmail.com>
2015-12-07 10:50:37 +0100

    next level

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter.messagebox import showinfo, showwarning, showerror
+from tkinter 
 from map import *
 
 
@@ -12,14 +13,15 @@ class Main():
 		self.menubar = Menu(self.win)
 		self.filemenu = Menu(self.menubar, tearoff=0)
 		self.menubar.add_cascade(label="File", menu=self.filemenu)
-		self.filemenu.add_command(label="New", command=self.new)
+		self.filemenu.add_command(label="New game", command=self.new)
+		self.filemenu.add_command(label="Open level", command=self.new)
 		self.filemenu.add_separator()
 		self.filemenu.add_command(label="Exit", command=self.win.destroy)
 		self.helpmenu = Menu(self.menubar, tearoff=0)
 		self.menubar.add_cascade(label="Help", menu=self.helpmenu)
 		self.helpmenu.add_command(label="About", command=self.about)		
 		self.win.config(menu=self.menubar)
-		self.map = Canvas(self.win, width=500, height=500)
+		self.new()
 		self.map.pack()
 		#Dimensions
 		self.win.geometry('640x480')
@@ -28,19 +30,24 @@ class Main():
 	def run(self):
 		self.win.mainloop()		
 	
-	def new(self):
-		pass
+	def new(self, level = 1):
+		self.level = level
+		filename = "niveau" + level + ".txt"
+		self.map = Map(self.win, filename, width=500, height=500)
 	
 	def about(self):
 		showinfo('Information sur le programme','v0.1, PA10')
 
 	def update(self):
 		try:
-			self.map.update(134)
+			self.map.update(134) #frequence de rafraichissement
 		except ValueError:
-			showinfo("Youuuuu WIN !")
+			level = self.level + 1
+			showinfo("Youuuuu WIN ! Next level : " + level)
+			self.new(level)
 		except KeyError:
 			showerror("You're dead, bitch !")
+			self.new()
 
 		self.map.show()
 		self.win.after(134, update)
